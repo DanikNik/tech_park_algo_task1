b7744386bf0ab6fa912d80fde9ce24ba37d4c37f
DanikNik <sway9009>
2018-10-26 13:13:16 +0300

    passed 6_2

--- dz5_2.cpp
+++ dz5_2.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 
+int* merge(int* arr1, int* arr2, int arr1_size, int arr2_size){
+
+}
+
+void merge_sort(int* arr, int left_b, int rigth_b){
+    if (rigth_b - left_b <=1){
+        return;
+    }
+
+    int median = (rigth_b + left_b)/2;
+    merge_sort(arr, left_b, median);
+    merge_sort(arr, median, rigth_b);
+
+    int* merged_arr = merge();
+}
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+
+
     return 0;
 }
--- dz6_2.cpp
+++ dz6_2.cpp
@@ -1,103 +1,67 @@
 /*
- * Даны неотрицательные целые числа n,k и массив целых чисел из [0..10^9] размера n. Требуется найти k-ю порядковую статистику. т.е. напечатать число, которое бы стояло на позиции с индексом k (0..n-1) в отсортированном массиве. Напишите нерекурсивный алгоритм.
+ *6_2  Даны неотрицательные целые числа n,k и массив целых чисел из [0..10^9] размера n. Требуется найти k-ю порядковую статистику. т.е. напечатать число, которое бы стояло на позиции с индексом k (0..n-1) в отсортированном массиве. Напишите нерекурсивный алгоритм.
 Требования к дополнительной памяти: O(n). Требуемое среднее время работы: O(n).
 Функцию Partition следует реализовывать методом прохода двумя итераторами в одном направлении. Описание для случая прохода от начала массива к концу:
 *
  */
 
 
-
-
-
 #include <iostream>
 #include <cassert>
+//#include <algorithm>
+
 using namespace std;
 
 
-//template<class T>
 void _swap(int &a, int &b) {
     int tmp = a;
     a = b;
     b = tmp;
 }
 
-
-//template<class int>
 int median(const int *arr, int begin, int end) {
-//    assert(begin < end);
-//    if (arr[begin] > arr[end - 1]) {
-//        if (arr[(begin + end) / 2] > arr[begin])
-//            return begin;
-//        return (arr[end - 1] > arr[(begin + end) / 2]) ? end - 1 : (end + begin) / 2;
-//    }
-//    if (arr[(begin + end) / 2] > arr[end - 1])
-//        return end - 1;
-//    return (arr[begin] > arr[(begin + end) / 2]) ? begin : (end + begin) / 2;
-    return (begin + end) / 2;
-}
-
-
-//template<class T>
-//int partition(int *arr, int begin, int end) {
-//    int pivot = median(arr, begin, end);
-//    _swap(arr[begin], arr[pivot]);
-//
-//    int i = end - 1;
-//    int j = end - 1;
-//
-//    while (j > begin) {
-//        if (arr[j] < arr[begin]) {
-//            j--;
-//        } else {
-//            _swap(arr[i], arr[j]);
-//            i--;
-//            j--;
-//        }
-//    }
-//
-//    _swap(arr[begin], arr[i]);
-//
-//    return i;
-//}
-
-//10 0
-//3 6 5 7 2 9 8 10 4 1
-
-
-
-int partition (int *arr, int begin, int end)
-{
-    int pivot = arr[(begin+end)/2];    // pivot
-    int i = (begin - 1);  // Index of smaller element
-
-    for (int j = begin; j <= end- 1; j++)
-    {
-        // If current element is smaller than or
-        // equal to pivot
-        if (arr[j] <= pivot)
-        {
-            i++;    // increment index of smaller element
-            _swap(arr[i], arr[j]);
-        }
+    if (arr[begin] > arr[end - 1]) {
+        if (arr[(begin + end) / 2] > arr[begin])
+            return begin;
+        return (arr[end - 1] > arr[(begin + end) / 2]) ? end - 1 : (end + begin) / 2;
     }
-    _swap(arr[i + 1], arr[end]);
-    return (i + 1);
+    if (arr[(begin + end) / 2] > arr[end - 1])
+        return end - 1;
+    return (arr[begin] > arr[(begin + end) / 2]) ? begin : (end + begin) / 2;
 }
 
 
+int partition(int *arr, int begin, int end) {
+    int pivot = median(arr, begin, end);
 
+    _swap(arr[pivot], arr[end - 1]);
 
+    int i = end - 2;
+    int j = end - 1;
+
+    while (j >= begin) {
+        if (arr[j] > arr[end - 1]) {
+            _swap(arr[i], arr[j]);
+            i--;
+        }
+        j--;
+    }
+    _swap(arr[++i], arr[end - 1]);
+
+    return i;
+}
 
 
 int k_statistic(int *arr, int begin, int end, int k) {
-    assert(begin < end);
-
     int pivot = partition(arr, begin, end);
-    if (pivot > k) {
-        return k_statistic(arr, begin, pivot, k);
-    }
-    if (pivot < k) {
-        return k_statistic(arr, pivot, end, k);
+    while (pivot != k) {
+        if (pivot > k) {
+            end = pivot;
+            pivot = partition(arr, begin, end);
+        } else if (pivot < k) {
+            begin = pivot;
+            pivot = partition(arr, begin, end);
+        }
     }
 
     return arr[pivot];
@@ -113,6 +77,6 @@ int main() {
     }
 
     cout << k_statistic(arr, 0, n, k);
-
+    delete[] arr;
     return 0;
 }
